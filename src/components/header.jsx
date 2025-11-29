edebd1589257fb8827b0c9e410d496e4ef9166ed
Vihanga Tharuka <vihangatd99@gmail.com>
2025-11-30 01:30:00 +0530

    update admin component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,16 @@
+import { Link } from "react-router-dom";
 import UserData from "./userData";
 
 export default function Header(){
     return(
-        <header className="w-full h-[100px] bg-accent">
-            <img src="/logo.png" className="w-[100px]" alt="" />
+        <header className="w-full h-[100px] bg-accent flex">
+            <img src="/logo.png" className="h-full " alt="" />
+            <div className="w-full h-full flex justify-center items-center gap-[30px] text-xl">
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/about">About</Link>
+                <Link to="/contact">Contact</Link>
+            </div>
         </header>
     )
 }
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
 
 export default function HomePage(){
     return(
-        <div className="text-secondary">
+        <div className="text-secondary w-full h-full overflow-y-scroll max-h-full">
             <Header/>
-            <h1>Home page</h1>
+            <div className="w-full min-h-[calc(100%-100px)] bg-red-500">
+                <Routes>
+                    <Route path="/" element={<h1>Home Page</h1>}/>
+                    <Route path="/products" element={<h1>Products Page</h1>}/>
+                    <Route path="/about" element={<h1>About Page</h1>}/>
+                    <Route path="/contact" element={<h1>Contact Page</h1>}/>
+                    <Route path="/*" element={<h1>Page not found</h1>}/>
+                </Routes>
+            </div>
         </div>
     )
 }
